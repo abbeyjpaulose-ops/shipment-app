8b25ae7d82d8d673a41a5c004b70859079f0a619
abbey <abbeyjohnsonp@gmail.com>
2025-09-10 15:40:40 +0530

    Added drop down for 4 components in the sidebar menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  username: string | null = '';
+  username: string | null = localStorage.getItem('username');
 
-  constructor() {
-    this.username = localStorage.getItem('username'); // read stored username
+  // Dropdown states
+  showShipments = false;
+  showAdmin = false;
+  showSettings = false;
+  showProfile = false;
+
+  toggleMenu(menu: string) {
+    if (menu === 'shipments') this.showShipments = !this.showShipments;
+    if (menu === 'admin') this.showAdmin = !this.showAdmin;
+    if (menu === 'settings') this.showSettings = !this.showSettings;
+    if (menu === 'profile') this.showProfile = !this.showProfile;
   }
 }
